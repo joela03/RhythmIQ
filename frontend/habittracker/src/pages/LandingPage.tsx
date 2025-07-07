b2f527078f549d86592fc97eeb20a80e875bf890
joela03 <joelallenc03@gmail.com>
2025-07-07 12:33:27 +0100

    added navbar to landing page

--- frontend/habittracker/src/pages/LandingPage.tsx
+++ frontend/habittracker/src/pages/LandingPage.tsx
@@ -1,8 +1,11 @@
+import Navbar from '../components/Navbar'
+
 const LandingPage = () => {
     return (
-        <>
+        <div className='min-h-screen bg-gray-50'>
+            <Navbar/>
             <h1>Hello World</h1>
-        </>
+        </div>
     )
 }
 
