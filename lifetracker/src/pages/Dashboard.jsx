cc0dedfe2ad98955886cd56a44348cc8d24862e9
joela03 <joelallenc03@gmail.com>
2025-08-15 23:58:08 +0100

    changed from flex to grid for main section

--- lifetracker/src/pages/Dashboard.jsx
+++ lifetracker/src/pages/Dashboard.jsx
@@ -1,11 +1,34 @@
-import Sidebar from '../components/Sidebar'
+import Sidebar from '../components/Sidebar';
+import SocialSection from '../components/SocialSection';
+import AIInsights from '../components/AIInsights';
+import StreakCalendar from '../components/StreakCalendar';
+import TodaysFocusCard from '../components/TodaysFocusCard';
 
 const Dashboard = () => {
-    return (
-        <>
-            <Sidebar></Sidebar>  
-        </>
-    )
-}
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <main className="flex-1 min-w-0 px-6 py-4">
+        <div className="grid grid-cols-12 gap-6 h-full">
+          <div className="col-span-4 grid-widget">
+            <AIInsights />
+          </div>
+          
+          <div className="col-span-4 grid-widget">
+            <StreakCalendar />
+          </div>
+        </div>
+        
+        <div className="flex items-start">
+            <div>
+                <TodaysFocusCard />
+            </div>
+        </div>
+      </main>
+      
+      <SocialSection />
+    </div>
+  );
+};
 
 export default Dashboard;
